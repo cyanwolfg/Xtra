097d3d53257ca5d8f8d62cdf635f398cd0877ff9
crackededed <90209774+crackededed@users.noreply.github.com>
2022-07-30 00:24:42 +0300

    pubsub viewer count
    (cherry picked from commit 6b96536a5fb1739ba5dc9b536640c4cb54f5727b)

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/player/offline/OfflinePlayerViewModel.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/player/offline/OfflinePlayerViewModel.kt
@@ -21,6 +21,11 @@ class OfflinePlayerViewModel @Inject constructor(
     private lateinit var video: OfflineVideo
     val qualities = mutableListOf(context.getString(R.string.source), context.getString(R.string.audio_only))
 
+    init {
+        val speed = context.prefs().getFloat(C.PLAYER_SPEED, 1f)
+        setSpeed(speed)
+    }
+
     fun setVideo(video: OfflineVideo) {
         val context = getApplication<Application>()
         if (!this::video.isInitialized) {
